3c84ba1b6ba638051c5dfb31582bb2c3416115d2
Matthew Caixeiro <Warlord5122.valor@gmail.com>
2016-03-26 16:54:40 -0400

    Basic Demo Done

--- src/InertiaUI.java
+++ src/InertiaUI.java
@@ -149,8 +149,8 @@ public class InertiaUI extends Application {
 				param[i] = recVec.elementAt(i);
 			}
 			
-			double MOI = 6; //TODO change this to Rachel's code
-			outputText.setText("The moment of Intertia for this system is " + MOI);
+			double MOI = InertiaCalc(param);
+			outputText.setText("The moment of Inertia for this system is " + MOI);
 		});
 		
 		reset.setOnAction(e3 -> {
@@ -166,6 +166,7 @@ public class InertiaUI extends Application {
 				}
 			}
 			
+			outputText.setText("");
 			recVec.clear();
 		});
 		
@@ -182,4 +183,41 @@ public class InertiaUI extends Application {
 		launch(args);
 	}
 
+	public double InertiaCalc(Rectangle[] rectangleArray){
+		int length = rectangleArray.length;
+		double[] Yarray = new double [length];
+		double Ytop = 0;
+		double[] widtharray = new double [length];
+		double[] heightarray = new double [length];
+		double[] areaarray = new double [length];
+		double NAnum = 0;
+		double NAdenom = 0;
+		double Inertia = 0;
+		
+		for(int i = 0; i <= (length-1); i++){
+			Yarray[i] = rectangleArray[i].getY();
+			
+			if( Ytop < Yarray[i]){
+				Ytop = Yarray[i];
+			}
+			
+			heightarray[i] = rectangleArray[i].getHeight();
+			widtharray[i] = rectangleArray[i].getWidth();
+			areaarray[i] = (heightarray[i]*widtharray[i]);
+		}
+		
+		for(int i = 0; i<= (length-1); i++){
+			NAnum = NAnum + areaarray[i]*(Ytop-Yarray[i]+heightarray[i]/2);
+			NAdenom = NAdenom +areaarray[i];
+		}
+		
+		double YNeutralAxis = Ytop-NAnum/NAdenom;
+		
+		for(int i = 0; i<= (length-1); i++){
+			double deltaY = YNeutralAxis - (Yarray[i]-heightarray[i]/2);
+			Inertia = Inertia + (1/12)*areaarray[i]*(heightarray[i])*(heightarray[i]) + areaarray[i]*deltaY*deltaY;
+		}
+		
+		return Inertia;
+	}
 }
