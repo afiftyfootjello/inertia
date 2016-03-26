edfe338cc5fa5f034b18c72cdec6d9941615ee89
Kyle Cochran <afiftyfootjello@gmail.com>
2016-03-26 13:58:59 -0400

    added MOI calculations to Polygon class

--- src/Polygon.java
+++ src/Polygon.java
@@ -1,11 +1,13 @@
 import java.awt.Point;
 import java.util.ArrayList;
 
-
+//So this class needs to be constructed with an ArrayList of Point objects. Upon construction, it automatically finds its own area and MOI
 
 public class Polygon {
 
 	public double area;
+	public double moiX;
+	public double moiY;
 	public double naXDist;
 	public double naYDist;
 	private ArrayList<Point> points;
@@ -17,6 +19,9 @@ public class Polygon {
 		this.points = points;
 
 		this.area = findArea(points);
+		this.moiX = findMOIX(points);
+		this.moiY = findMOIY(points);
+		
 	}
 	
 	
@@ -32,6 +37,24 @@ public class Polygon {
 		
 	}
 	
+	private double findMOIX(ArrayList<Point> points){
+		double moi = 0;
+		
+		for(int i=0; i<points.size(); i++){
+			moi=((points.get(i).y)^2+(points.get(i).y)*(points.get(i+1).y)+(points.get(i+1).y)^2)+((points.get(i).x)*(points.get(i+1).y)-(points.get(i+1).x)*(points.get(i).y));
+					
+		}
+		return moi;
+	}
 	
+	private double findMOIY(ArrayList<Point> points){
+		double moi = 0;
+		
+		for(int i=0; i<points.size(); i++){
+			moi=((points.get(i).x)^2+(points.get(i).x)*(points.get(i+1).x)+(points.get(i+1).x)^2)+((points.get(i).x)*(points.get(i+1).y)-(points.get(i+1).x)*(points.get(i).y));		
+		}
+		
+		return moi;
+	}
 
 }
